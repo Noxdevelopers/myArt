98a2140067e9f574fe853e3bbbdc040e9a36ed9e
hasan <hasan.hpe72@gmail.com>
2021-01-10 23:25:25 +0330

    this is first change

--- ROUTE.php
+++ ROUTE.php
@@ -8,10 +8,15 @@ define("USER_ID"         , "ID");
 define("ROUTE_USERS"     , "USERS");
 define("ACTION_LOGIN"    , "LOGIN");
 define("ACTION_REGISTER" , "REGISTER");
+define("INPUT_EMAIL"     , "EMAIL");//h
+define("INPUT_PHONE"     , "PHONE");//h
+define("INPUT_FULLNAME"  , "FULLNAME");//h
 //Products
 define("ROUTE_PRODUCTS"  , "PRODUCTS");
 define("ACTION_ADD"      , "ADD");
 define("ACTION_READ"     , "READ");
 define("ACTION_EDIT"     , "EDIT");
 define("ACTION_DELETE"   , "DELETE");
+define("INPUT_TITLE"     , "TITLE");//h
+define("INPUT_MESSAGE"   , "MESSAGE");//h
 define("INPUT_IMAGE"     , "IMAGE");
--- classes/users.php
+++ classes/users.php
@@ -1,5 +1,30 @@
 <?php
 
-class users {
+class users
+{
+
+    public function __construct($system = false)
+    {
+
+        if ($system) return;
+
+        $action = app::get(ACTION);
+
+        switch ($action) {
+            case ACTION_LOGIN:
+            {
+
+
+
+            }
+
+            case ACTION_REGISTER :
+            {
+
+
+
+            }
+        }
+    }
 
 }
