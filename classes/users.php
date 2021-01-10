f51ed3067d44d7eb75868573e2c05c50a36d06b1
hasan <hasan.hpe72@gmail.com>
2021-01-11 00:20:58 +0330

    this is first change

--- classes/users.php
+++ classes/users.php
@@ -14,17 +14,50 @@ class users
             case ACTION_LOGIN:
             {
 
+                $email = app::get(INPUT_EMAIL);
+                $phone = app::get(INPUT_PHONE);
 
+                $this->login($email, $phone);
+
+                break;
 
             }
 
             case ACTION_REGISTER :
             {
 
+                $email = app::get(INPUT_EMAIL);
+                $phone = app::get(INPUT_PHONE);
+                $fullname = app::get(INPUT_FULLNAME);
 
+                $this->register($email, $phone, $fullname);
+
+                break;
 
             }
         }
+    }
+
+    public function login($email, $phone, $system = false){
+
+
+
+    }
+
+    public function register($email, $phone, $fullname, $system = false)
+    {
+
+        $error = new MyError();
+        if ($email == -1 || $phone == -1 || !filter_var($email, FILTER_VALIDATE_EMAIL)){
+
+            $error->display("Invalid Data", "", MyError::$ERROR_INVALID_DATA);
+
+        }
+
+        $conn = MyPDO::getInstance();
+
+
+
     }
 
 }
